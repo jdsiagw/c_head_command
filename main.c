2cbea237d19ea46f60f0c2ab56ab2c800d07afdf
jds <jdsiagw@gmail.com>
2019-11-15 17:25:53 +0300

    need to add minus

--- main.c
+++ main.c
@@ -5,44 +5,147 @@
 #include <fcntl.h>
 #include <stdbool.h>
 #include <string.h>
-#include "mpp2file.h"
+//#include "mpp2file.h"
 
-#define BUFSIZE 512
+#define BUFSIZE 4096
 
-void nHead(char *file, long numberOfRows) {
-    int count = 0;
-    if (numberOfRows == -1) {
-        numberOfRows = 10;
+void print(char *str) {
+    size_t length = strlen(str);
+    size_t size = 0;
+    while (size < length) {
+        size_t s = write(STDOUT_FILENO, str + size, length - size);
+        if (s == -1) {
+            return;
+        }
+        size += s;
     }
-    char *line = (char *) malloc(BUFSIZE);
-    off_t offset = 0;
-    while (readline(line, BUFSIZE, file, &offset) != -1 && count < numberOfRows) {
-        write(STDOUT_FILENO, line, strlen(line));
-        write(STDOUT_FILENO, "\n", 1);
-        count++;
-    }
-    free(line);
 }
 
-void cHead(char *file, long numberOfChars) {
+void printError(char *str) {
+    size_t length = strlen(str);
+    size_t size = 0;
+    while (size < length) {
+        size_t s = write(STDERR_FILENO, str + size, length - size);
+        if (s == -1) {
+            return;
+        }
+        size += s;
+    }
+}
+
+ssize_t openFile(char *fileName) {
+    int fd = open(fileName, O_RDONLY);
+    if (fd == -1) {
+        return ENOENT;
+    }
+    return fd;
+}
+
+int countLinesOfFile(char *filename) {
+    char *buffer = (char *) malloc(BUFSIZE);
+    int bytes_read;
+    int lines = 0;
+    int fd = openFile(filename);
+    if (fd == ENOENT) {
+        printError(strerror(ENOENT));
+        return ENOENT;
+    }
+    bytes_read = read(fd, buffer, sizeof(buffer));
+    while (bytes_read) {
+        for (int i = 0; i < bytes_read; ++i) {
+            if (buffer[i] == '\n' || buffer[i] == 0x0) {
+                lines++;
+            }
+        }
+        bytes_read = read(fd, buffer, sizeof(buffer));
+    }
+    free(buffer);
+    return lines;
+}
+
+long countCharsOfFile(char *filename) {
+    char *buffer = (char *) malloc(BUFSIZE);
+    int bytes_read;
     int count = 0;
-    int fd;
-    if ((fd = open(file, O_RDONLY)) >= 0) {
-        char c;
-        while (read(fd, &c, 1) == 1 && count < numberOfChars) {
-            write(STDOUT_FILENO, &c, 1);
+    int fd = openFile(filename);
+    if (fd == ENOENT) {
+        printError(strerror(ENOENT));
+        return ENOENT;
+    }
+    bytes_read = read(fd, buffer, sizeof(buffer));
+    while (bytes_read) {
+        for (int i = 0; i < bytes_read; ++i) {
             count++;
         }
+        bytes_read = read(fd, buffer, sizeof(buffer));
+    }
+    return count;
+}
+
+void nHead(char *filename, long numberOfRows) {
+    char *buffer = (char *) malloc(BUFSIZE);
+    char buf[1] = {0};
+    int bytes_read;
+    int lines = 0;
+
+    int fd = openFile(filename);
+    if (fd == ENOENT) {
+        printError(strerror(ENOENT));
+        return;
+    }
+    bytes_read = read(fd, buffer, sizeof(buffer));
+    while (bytes_read && lines < numberOfRows) {
+        for (int i = 0; i < bytes_read; ++i) {
+            buf[0] = buffer[i];
+            print(buf);
+            if (buffer[i] == '\n' || buffer[i] == 0x0) {
+                lines++;
+                if (lines == numberOfRows) {
+                    break;
+                }
+            }
+        }
+        bytes_read = read(fd, buffer, sizeof(buffer));
+    }
+    free(buffer);
+}
+
+void cHead(char *filename, long numberOfChars) {
+    char *buffer = (char *) malloc(BUFSIZE);
+    int bytes_read;
+    int count = 0;
+    char buf[1] = {0};
+    int fd = openFile(filename);
+    if (fd == ENOENT) {
+        printError(strerror(ENOENT));
+        printError("\n");
+        return;
+    }
+    bytes_read = read(fd, buffer, sizeof(buffer));
+    while (bytes_read && count < numberOfChars) {
+        for (int i = 0; i < bytes_read; ++i) {
+            buf[0] = buffer[i];
+            print(buf);
+            count++;
+            if (count == numberOfChars) {
+                break;
+            }
+        }
+        bytes_read = read(fd, buffer, sizeof(buffer));
     }
 }
 
 void vHead(char *fileName) {
-    write(STDOUT_FILENO, "==>", 3);
-    write(STDOUT_FILENO, fileName, strlen(fileName));
-    write(STDOUT_FILENO, "<==\n", 4);
+    print("==>");
+    print(fileName);
+    print("<==");
 }
 
 int main(int argc, char *argv[]) {
+    if (argc == 0) {
+
+    }
+//    printError("123\n321");
     int option;
     long rows = 10;
     long chars = 0;
@@ -55,9 +158,9 @@ int main(int argc, char *argv[]) {
                 chars = 0;
                 rows = strtol(optarg, &endptr, 0);
                 if (errno == ERANGE || *endptr != '\0' || optarg == endptr) {
-                    write(STDOUT_FILENO, "head: invalid number of lines: ", 31);
-                    write(STDOUT_FILENO, optarg, strlen(optarg));
-                    write(STDOUT_FILENO, "\n", 1);
+                    printError("head: invalid number of lines: ");
+                    printError(optarg);
+                    printError("\n");
                     return 1;
                 }
                 break;
@@ -65,44 +168,52 @@ int main(int argc, char *argv[]) {
                 rows = 0;
                 chars = strtol(optarg, &endptr, 0);
                 if (errno == ERANGE || *endptr != '\0' || optarg == endptr) {
-                    write(STDOUT_FILENO, "head: invalid number of bytes: ", 31);
-                    write(STDOUT_FILENO, optarg, strlen(optarg));
-                    write(STDOUT_FILENO, "\n", 1);
+                    printError("head: invalid number of bytes: ");
+                    printError(optarg);
+                    printError("\n");
                     return 1;
                 }
                 break;
             case 'q':
                 qFlag = true;
                 vFlag = false;
-                write(STDOUT_FILENO, "q\n", 2);
+//                write(STDOUT_FILENO, "q\n", 2);
                 break;
             case 'v':
                 vFlag = true;
                 qFlag = false;
-                write(STDOUT_FILENO, "v\n", 2);
+//                write(STDOUT_FILENO, "v\n", 2);
                 break;
+            default:
+                return 0;
         }
     }
     for (; optind < argc; optind++) {
         char *fileName = argv[optind];
 
-        if (argc - optind > 1) {
+        if (argc - optind > 1 && qFlag == false) {
             vFlag = true;
-            qFlag = false;
         }
-
         if (vFlag && !qFlag) {
             vHead(fileName);
         }
-        if (rows > 0) {
+        if (rows >= 0) {
             nHead(fileName, rows);
         }
-        if (chars > 0) {
-            cHead(fileName, chars);
+        if (rows < 0) {
+            long totalCount = countLinesOfFile(fileName);
+            nHead(fileName, totalCount + rows);
         }
-        write(STDOUT_FILENO, "\n", 1);
+        if (chars >= 0) {
+            long totalCount = 0;
+            cHead(fileName, totalCount + chars);
+        }
+        if (chars < 0) {
+            long totalCount = countCharsOfFile(fileName);
+            cHead(fileName, totalCount + rows);
+        }
+        print("\n");
     }
 
     return 0;
 }
-
--- makefile
+++ makefile
@@ -1,7 +1,7 @@
 all: head
 
 head: main.o mpp2file.o
-	gcc -Wall -Wextra -std=gnu99 -pedantic -Werror main.o mpp2file.o -o head
+	gcc -Wall -Wextra -std=gnu99 -pedantic -Werror main.o mpp2file.o
 
 main.o: main.c
 	gcc -c main.c
